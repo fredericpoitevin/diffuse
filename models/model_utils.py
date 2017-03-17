f8e6c4e5c30370425ef680a6e84eee395075f0f2
Ariana Peck <apeck@vspm12.stanford.edu>
2017-03-17 16:18:20 -0700

    updated model_utils.py

--- models/model_utils.py
+++ models/model_utils.py
@@ -2,8 +2,32 @@ from itertools import islice
 import cPickle as pickle
 import numpy as np
 import itertools, time
+import mdtraj as md
 
 """ Miscellaneous functions useful for analyzing diffuse maps. """
+
+def split_unitcell(uc_pdb, n_asus):
+    """
+    Split asymmetric units of input unit cell and return as separate entries in output
+    dictionary.
+    """
+
+    # stack asus if written out as separate models in input pdb
+    if uc_pdb.n_frames > 1:
+        processed = uc_pdb[0]
+        for i in range(1, uc_pdb.n_frames):
+            processed = processed.stack(uc_pdb[i])
+    else:
+        processed = uc_pdb
+
+    asus = dict()
+    n_atoms = processed.n_atoms/n_asus
+
+    for i in range(n_asus):
+        asus[i] = processed.atom_slice(range(n_atoms*i, n_atoms*(i+1)))
+
+    return asus
+
 
 def generate_symmates(symm_ops, bins, subsampling):
     """
@@ -41,6 +65,7 @@ def generate_symmates(symm_ops, bins, subsampling):
 
     return symm_idx, grid, multiplicities
 
+
 def symmetrize(input_map, symm_idx, from_asu = False):
     """
     Symmetrize input map according to the symmetry-equivalent indices encoded in symm_idx;
@@ -65,11 +90,12 @@ def symmetrize(input_map, symm_idx, from_asu = False):
 
     return symm_map
 
+
 def generate_mesh(A, bins):
     """
     Generate qvector mesh for use with plt.pcolormesh from A matrix and bins.
     """
-    A_inv = np.linagl.inv(A)
+    A_inv = np.linalg.inv(A)
 
     mesh = dict()
     mesh['projX'] = np.meshgrid(2*np.pi*np.dot(A_inv[2][2], bins['l']), 2*np.pi*np.dot(A_inv[1][1], bins['k']))
@@ -77,6 +103,7 @@ def generate_mesh(A, bins):
     mesh['projZ'] = np.meshgrid(2*np.pi*np.dot(A_inv[1][1], bins['k']), 2*np.pi*np.dot(A_inv[0][0], bins['h']))
 
     return mesh
+
 
 def mweighted_cc(map1, map2, mult = None):
     """
@@ -102,3 +129,39 @@ def mweighted_cc(map1, map2, mult = None):
     map1_sel, map2_sel, mult_sel = map1_sel[valid_idx], map2_sel[valid_idx], mult[valid_idx]
 
     return w_cov(map1_sel, map2_sel, mult_sel) / np.sqrt(w_cov(map1_sel, map1_sel, mult_sel) * w_cov(map2_sel, map2_sel, mult_sel))
+
+
+def compute_resolution(space_group, cell_constants, s_grid):
+    """
+    Compute d-spacing / resolution for a set of scattering vectors, where q = 2*pi*s.
+    Set (0,0,0) to arbitrarily high (but not infinite) resolution.
+    Inputs: np.array of cell constants in order (a, b, c, alpha, beta, gamma),
+            space group (number), grid of scattering vectors
+    Output: np.array of d-spacings, which is empty if space group is unsupported.
+    """
+
+    a, b, c, alpha, beta, gamma = cell_constants
+    h, k, l = s_grid[:,0], s_grid[:,1], s_grid[:,2]
+
+    # valid for orthorhombic, cubic, and tetragonal
+    if ((space_group >= 16) and (space_group <=142)) or ((space_group >= 195) and (space_group <=230)):
+        inv_d = np.sqrt(np.square(h/a) + np.square(k/b) + np.square(l/c))
+
+    # valid for hexagonal (and possibly trigonal? if so change lower bound to 143)
+    elif (space_group >= 168) and (space_group <=194):
+        inv_d = np.sqrt(4.0*(np.square(h) + h*k + np.square(k))/(3*np.square(a)) + np.square(l/c))
+
+    # valid for monoclinic
+    elif (space_group >= 3) and (space_group <=15):
+        beta = np.deg2rad(beta)
+        inv_d = np.sqrt( np.square(h/(a*np.sin(beta))) + np.square(k/b) + np.square(l/(c*np.sin(beta)))\
+                             + 2*h*l*np.cos(beta) / (a*c*np.square(np.sin(beta))))
+
+    else:
+        print "This space group is currently unsupported. Please add 'bins' key manually to systems.pickle."
+        return np.empty(0)
+
+    inv_d[inv_d==0] = 1e-5
+    res = 1.0 / inv_d
+
+    return res
