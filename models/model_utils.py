2f92a4298ce9eddcdb4bef6406c3330c975622b7
Ariana Peck <apeck@stanford.edu>
2017-03-31 21:46:31 -0700

    updated model_utils.py

--- models/model_utils.py
+++ models/model_utils.py
@@ -91,44 +91,46 @@ def symmetrize(input_map, symm_idx, from_asu = False):
     return symm_map
 
 
-def generate_mesh(system, bins):
+def generate_mesh(system):
     """
     Generate qvector mesh for use with plt.pcolormesh from A matrix and bins.
     """
     A_inv = np.linalg.inv(np.diag(system['cell'][:3]))
 
     mesh = dict()
-    mesh['projX'] = np.meshgrid(2*np.pi*np.dot(A_inv[2][2], bins['l']), 2*np.pi*np.dot(A_inv[1][1], bins['k']))
-    mesh['projY'] = np.meshgrid(2*np.pi*np.dot(A_inv[2][2], bins['l']), 2*np.pi*np.dot(A_inv[0][0], bins['h']))
-    mesh['projZ'] = np.meshgrid(2*np.pi*np.dot(A_inv[1][1], bins['k']), 2*np.pi*np.dot(A_inv[0][0], bins['h']))
+    mesh['projX'] = np.meshgrid(2*np.pi*np.dot(A_inv[2][2], system['bins']['l']), 2*np.pi*np.dot(A_inv[1][1], system['bins']['k']))
+    mesh['projY'] = np.meshgrid(2*np.pi*np.dot(A_inv[2][2], system['bins']['l']), 2*np.pi*np.dot(A_inv[0][0], system['bins']['h']))
+    mesh['projZ'] = np.meshgrid(2*np.pi*np.dot(A_inv[1][1], system['bins']['k']), 2*np.pi*np.dot(A_inv[0][0], system['bins']['h']))
 
     return mesh
 
 
-def mweighted_cc(map1, map2, mult = None):
+def cc_by_shell(system, n_shells, map1, map2):
     """
-    Compute correlation coefficient between maps. Valid (positive, nonzero) intensity values are 
-    weighted by their multiplicity; it is assumed that input maps exhibit the same symmetry.
+    Compute multiplicity-weighted correlation coefficient across n_shells resolution shells 
+    (with spacing even in 1/d^3); return np.arrays of CC and median resolution of shells.
     """
 
-    def w_mean(x, weights):
-        return np.sum(x * weights) / np.sum(weights)
+    hkl_grid = np.array(list(itertools.product(system['bins']['h'], system['bins']['k'], system['bins']['l'])))
+    res = compute_resolution(system['space_group'], system['cell'], hkl_grid)
 
-    def w_cov(x, y, weights):
-        return np.sum(weights * (x - w_mean(x, weights)) * (y - w_mean(y, weights))) / np.sum(weights)
+    inv_dcubed = 1.0/(res**3.0)
+    nvox_per_shell, shell_bounds = np.histogram(inv_dcubed[res > system['d']], bins = n_shells)
 
-    # process multiplicities array, generating one with equal weights if not given
-    if mult is None:
-        mult = np.ones(map1.shape).flatten()
-    mult = 1.0/mult
-    mult /= np.sum(mult)
+    assert system['d'] > 1.0
+    shell_bounds = np.concatenate((shell_bounds, [1.0]))
+    vx_dig = np.digitize(inv_dcubed, shell_bounds)
+    res_bins = (1.0/shell_bounds[:-1])**(1.0/3)
 
-    # only consider voxels with valid (positive) intensities
-    map1_sel, map2_sel = map1.copy().flatten(), map2.copy().flatten()
-    valid_idx = np.where((map1_sel > 0) & (map2_sel > 0))[0]
-    map1_sel, map2_sel, mult_sel = map1_sel[valid_idx], map2_sel[valid_idx], mult[valid_idx]
+    cc_shell, res_shell = np.zeros(n_shells), np.zeros(n_shells)
+    map1, map2 = map1.flatten(), map2.flatten()
+    for i in range(1, n_shells+1):
+        idx = np.where(vx_dig==i)[0]
+        cc_shell[i-1] = mweighted_cc(map1[idx], map2[idx], mult = mult)
+        valid = reduce(np.intersect1d, (np.where(map1>0)[0], np.where(map2>0)[0], idx))
+        res_shell[i-1] = np.mean(res[reduce(np.intersect1d, (np.where(map1>0)[0], np.where(map2>0)[0], idx))])
 
-    return w_cov(map1_sel, map2_sel, mult_sel) / np.sqrt(w_cov(map1_sel, map1_sel, mult_sel) * w_cov(map2_sel, map2_sel, mult_sel))
+    return res_shell, cc_shell
 
 
 def compute_qmags(system):
@@ -193,7 +195,7 @@ def subtract_radavg(system, input_map, n_bins, medians = False):
 
     # compute resolution associated with voxels
     hkl = np.array(list(itertools.product(system['bins']['h'], system['bins']['k'], system['bins']['l'])))
-    hkl_res = map_utils.compute_resolution(system['space_group'], system['cell'], hkl)
+    hkl_res = compute_resolution(system['space_group'], system['cell'], hkl)
 
     # perform binning, linear in 1/d
     x, y = 1.0/hkl_res[input_map.flatten()>0], input_map.copy().flatten()[input_map.flatten()>0]
@@ -219,3 +221,55 @@ def subtract_radavg(system, input_map, n_bins, medians = False):
     return aniso_map
 
 
+def mweighted_cc(map1, map2, mult = None):
+    """ 
+    Compute correlation coefficient between maps. Valid (positive, nonzero) intensity values are 
+    weighted by their multiplicity; it is assumed that input maps exhibit the same symmetry. 
+    """
+
+    def w_mean(x, weights):
+        return np.sum(x * weights) / np.sum(weights)
+
+    def w_cov(x, y, weights):
+        return np.sum(weights * (x - w_mean(x, weights)) * (y - w_mean(y, weights))) / np.sum(weights)
+
+    # process multiplicities array, generating one with equal weights if not given
+    if mult is None:
+        mult = np.ones(map1.shape).flatten()
+    mult = 1.0/mult
+    mult /= np.sum(mult)
+
+    # only consider voxels with valid (positive) intensities                                                                                                              
+    map1_sel, map2_sel = map1.copy().flatten(), map2.copy().flatten()
+    valid_idx = np.where((map1_sel > 0) & (map2_sel > 0))[0]
+    map1_sel, map2_sel, mult_sel = map1_sel[valid_idx], map2_sel[valid_idx], mult[valid_idx]
+
+    return w_cov(map1_sel, map2_sel, mult_sel) / np.sqrt(w_cov(map1_sel, map1_sel, mult_sel) * w_cov(map2_sel, map2_sel, mult_sel))
+
+
+def cc_by_shell(system, n_shells, map1, map2, mult):
+    """  
+    Compute multiplicity-weighted correlation coefficient across n_shells resolution shells 
+    (with spacing even in 1/d^3); return np.arrays of CC and median resolution of shells. 
+    """
+
+    hkl_grid = np.array(list(itertools.product(system['bins']['h'], system['bins']['k'], system['bins']['l'])))
+    res = compute_resolution(system['space_group'], system['cell'], hkl_grid)
+
+    inv_dcubed = 1.0/(res**3.0)
+    nvox_per_shell, shell_bounds = np.histogram(inv_dcubed[res > system['d']], bins = n_shells)
+
+    assert system['d'] > 1.0
+    shell_bounds = np.concatenate((shell_bounds, [1.0]))
+    vx_dig = np.digitize(inv_dcubed, shell_bounds)
+    res_bins = (1.0/shell_bounds[:-1])**(1.0/3)
+
+    cc_shell, res_shell = np.zeros(n_shells), np.zeros(n_shells)
+    map1, map2 = map1.flatten(), map2.flatten()
+    for i in range(1, n_shells+1):
+        idx = np.where(vx_dig==i)[0]
+        cc_shell[i-1] = mweighted_cc(map1[idx], map2[idx], mult = mult)
+        valid = reduce(np.intersect1d, (np.where(map1>0)[0], np.where(map2>0)[0], idx))
+        res_shell[i-1] = np.mean(res[reduce(np.intersect1d, (np.where(map1>0)[0], np.where(map2>0)[0], idx))])
+
+    return res_shell, cc_shell
